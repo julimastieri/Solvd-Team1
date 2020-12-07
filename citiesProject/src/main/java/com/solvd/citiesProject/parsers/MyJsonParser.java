de92fc142eba9ce429be3e5b84cbebc5b28e85ab
Joshua <acciarrijoshua@gmail.com>
2020-12-07 20:55:03 -0300

    Remove unnecesary code and add DateFormatter to JSON Parser

--- citiesProject/src/main/java/com/solvd/citiesProject/parsers/MyJsonParser.java
+++ citiesProject/src/main/java/com/solvd/citiesProject/parsers/MyJsonParser.java
@@ -2,6 +2,8 @@ package com.solvd.citiesProject.parsers;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -22,36 +24,10 @@ public class MyJsonParser {
 	static Logger logger = LogManager.getLogger(MyJsonParser.class);
 	static ObjectMapper mapper = new ObjectMapper();
 	
-	/*public static void writeTest(String fileName) {
-		String path = "src/main/resources/" + fileName;
-		String jsonObj = null;
-		List<Path> paths = new ArrayList<Path>();
-		Path p = new Path(4, new Point(1, null, 5, 326, 431), new Point(2, null, 5, 32, 31), 1, true, new Transport());
-		Path p2 = new Path(10, new Point(3, null, 5, 322, 341), new Point(4, null, 2, 3337, 713), 2, true, new Transport());
-		p.getFrom().setConnections(null);
-		p.getTo().setConnections(null);
-		p2.getFrom().setConnections(null);
-		p2.getTo().setConnections(null);
-		paths.add(p);
-		paths.add(p2);
-		Trip trip = new Trip();
-		trip.setPaths(paths);
-		
-		try {
-			mapper.writerWithDefaultPrettyPrinter().writeValue(new File(path), trip);
-			
-		} catch (IOException e) {
-			logger.error(e);
-		}
-		try {
-		    jsonObj = FileUtils.readFileToString(new File(path), "UTF-8");
-		} catch (IOException e) {
-			logger.error(e);
-		}
-		logger.info("\nGenerated Json: \n" + jsonObj + "\n");
-	}*/
 	
 	public static void writeJsonFile(List<Path> paths, User client, String fileName) {
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		mapper.setDateFormat(df);
 		String path = "src/main/resources/" + fileName;
 		String jsonObj = null;
 		Trip trip = new Trip();
